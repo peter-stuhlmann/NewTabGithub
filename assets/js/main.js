d6fe0c98937cbcfd85052c5e37e2745f77ef9f4e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-11 14:21:59 +0100

    Added fetch (github repos API)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,35 @@ setInterval(function () {
     let currentTime = moment().format('LTS')
     document.querySelector("#currentTime").innerHTML = currentTime
 }, interval);
+
+
+function github_repositories() {
+    
+    fetch(`https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5`)
+        .then(
+            response => response.json()
+        )
+        .then(
+            repos => {
+                let repoList = [];
+                repos.slice(0,5).forEach(
+                    repo => {
+                        repoList.push(`
+                        <div class="repo">
+                            <a class="name" title="Redirect to github.com" target="_blanc" href="${repo.html_url}">${repo.name}</a><br>
+                            <span class="description">${repo.description}</span><br>
+                            <span><i class="fas fa-star">&nbsp;</i>${repo.stargazers_count}</span>
+                            <span><i class="fas fa-code-branch">&nbsp;</i>${repo.forks}</span>
+                        </div>                    
+                    `)
+                    }
+                )
+                document.querySelector('.github-repositories').innerHTML = repoList.join('')
+
+            },
+        )
+        .catch(
+            err => console.log(`panic: ${err}`)
+        )
+}
+github_repositories()

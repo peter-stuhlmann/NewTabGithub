e931a9acb1eb266392c42d98fbf822d8e2b589e9
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-12 12:39:35 +0100

    Created a class github and transformed function github_api to method

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,45 @@
+class github {
+    constructor() {
+        this.data = null
+
+        this.github_repositories()
+    }
+
+    github_repositories() {
+
+        fetch(`https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5`)
+            .then(
+                response => response.json()
+            )
+            .then(
+                repos => {
+                    this.update(repos)
+                    let repoList = []
+                    repos.forEach(repo =>
+                        repoList.push(`
+                                <div class="repo">
+                                    <a class="name" title="Redirect to github.com" target="_blanc" href="${repo.html_url}">${repo.name}</a><br>
+                                    <span class="description">${repo.description}</span><br>
+                                    <span><i class="fas fa-star">&nbsp;</i>${repo.stargazers_count}</span>
+                                    <span><i class="fas fa-code-branch">&nbsp;</i>${repo.forks}</span>
+                                </div>                    
+                            `)
+                    )
+                    document.querySelector('.github-repositories').innerHTML = repoList.join('')
+                },
+            )
+            .catch(
+                err => console.log(`panic: ${err}`)
+            )
+    }
+
+    update(data) {
+        console.log(data)
+        this.data = data
+    }
+}
+
+
 let interval = 1000;
 
 setInterval(function () {
@@ -6,33 +48,8 @@ setInterval(function () {
 }, interval);
 
 
-function github_repositories() {
-    
-    fetch(`https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5`)
-        .then(
-            response => response.json()
-        )
-        .then(
-            repos => {
-                let repoList = [];
-                repos.slice(0,5).forEach(
-                    repo => {
-                        repoList.push(`
-                        <div class="repo">
-                            <a class="name" title="Redirect to github.com" target="_blanc" href="${repo.html_url}">${repo.name}</a><br>
-                            <span class="description">${repo.description}</span><br>
-                            <span><i class="fas fa-star">&nbsp;</i>${repo.stargazers_count}</span>
-                            <span><i class="fas fa-code-branch">&nbsp;</i>${repo.forks}</span>
-                        </div>                    
-                    `)
-                    }
-                )
-                document.querySelector('.github-repositories').innerHTML = repoList.join('')
+function gitRepoFilter() {
 
-            },
-        )
-        .catch(
-            err => console.log(`panic: ${err}`)
-        )
 }
-github_repositories()
+
+const api = new github
